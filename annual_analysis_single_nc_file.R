a8fea2fdd65e599fe51bcb917b65ed6ff3b07ae8
Edwin Sutanudjaja <edwinhs@cartesius.surfsara.nl>
2018-10-26 02:06:01 +0200

    from edwinhs

--- annual_analysis_single_nc_file.R
+++ annual_analysis_single_nc_file.R
@@ -12,22 +12,25 @@ args <- commandArgs()
 # the netcdf file that will be analyzed:
 nc_file_name <- args[4]
 # - variable name
-nc_variable  <- args[4]
+nc_variable  <- args[5]
 
 # opening netcdf files:
-nc_file = nc_open( nc_file_name , sep = "") )
-
-# time values 
-time = ncvar_get(swt_file, "time"); length(time)
+print(nc_file_name)
+nc_file = nc_open(nc_file_name)
 
 # cell area 
 cell_area_file = nc_open("/home/edwin/data/cell_area_nc/cellsize05min.correct.used.nc")
 cell_area = ncvar_get(cell_area_file, "Band1")[,]
 nc_close(cell_area_file)
 
+year = seq(1955, 2010, 1)
+
+for (i in 1:length(year)){
+
 nc_field = ncvar_get(nc_file, nc_variable, c(1, 1, i), c(-1, -1, 1))
 
 # calculate the global values (unit: km3)
-global_value = sum( pre_field  * cell_area, na.rm = T)/10^9
+global_value = sum(nc_field  * cell_area, na.rm = T)/10^9
 
-print(paste("Global value (km3): ", global_value))
+print(paste("Global value (km3) ", year[i], " : ", global_value))
+}
